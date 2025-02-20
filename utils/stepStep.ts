53725ecf818ddd9293ce75886ed2b36f92f1c218
gnehs <gnehs@gnehs.net>
2025-02-20 17:49:42 +0800

    added ua

--- utils/stepStep.ts
+++ utils/stepStep.ts
@@ -84,6 +84,8 @@ export class StepStep {
     const headers = {
       Authorization: `Bearer ${this.token}`,
       "Content-Type": "application/json",
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
       ...options.headers,
     };
 
