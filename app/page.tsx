7b03b19c4e33a195aca7e8a125e08a5257cc3cae
gnehs <gnehs@gnehs.net>
2025-02-20 20:15:56 +0800

    added notice attr

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import { useEffect, useState } from "react";
 export default function Home() {
   return (
     <div className="flex h-[100svh] w-[100vw] flex-col items-center text-gray-900">
-      <div className="sticky top-0 w-full border-b border-gray-200 p-2 px-4">
+      <div className="sticky top-0 z-20 w-full border-b border-gray-200 bg-white/95 p-2 px-4 backdrop-blur">
         <div className="max-w-2xl">
           <div className="text-2xl font-bold tracking-tighter">
             Home Dashboard
           </div>
         </div>
       </div>
-      <div className="my-8 flex max-w-[514px] flex-col items-center gap-2">
+      <div className="flex max-w-[514px] flex-col items-center gap-2 py-8">
         <h1 className="w-full text-left text-2xl font-bold">
           歡迎來到 <span className="tracking-tighter">Home Dashboard</span>
         </h1>
@@ -42,7 +42,8 @@ export default function Home() {
         <Card
           title="家庭監測面板"
           url="/img/board-1"
-          description="連接 Home Assistant 與 StepStep，可即時顯示家庭狀況與步行資料，這個圖片需要進行 API Token 設定與進行修改，若您未設定可能會無法顯示。"
+          description="連接 Home Assistant 與 StepStep，可即時顯示家庭狀況與步行資料"
+          notice="這個主控板需要進行 API Host 與 token 設定與並針對您的 Home Assistant 裝置 ID 進行修改，若未進行上述步驟將無法正常顯示。"
         />
         <Card
           title="匯率"
@@ -62,10 +63,12 @@ function Card({
   title,
   url,
   description,
+  notice,
 }: {
   title: string;
   url: string;
   description: string;
+  notice?: string;
 }) {
   const [currentBaseURL, setCurrentBaseURL] = useState<string>("");
   useEffect(() => {
@@ -88,6 +91,11 @@ function Card({
       </div>
       <div className="mt-2 text-xl font-bold text-gray-900">{title}</div>
       <div className="text-gray-600">{description}</div>
+      {notice && (
+        <div className="mt-2 border-l-4 border-gray-200 pl-2 text-sm text-gray-400">
+          {notice}
+        </div>
+      )}
     </div>
   );
 }
